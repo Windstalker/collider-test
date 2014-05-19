d00e5afead55fe1de0ae538ee77c125220344371
Alexander Subota <a.subota@mobidev.biz>
2014-05-19 17:55:21 +0300

    basic animation and colliding implemented

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
+var tid = null;
+var INTERVAL = 100;
+var objects = [];
+var cnv = document.querySelector('#test'),
+    ctx = cnv.getContext('2d');
 
+var collider = new Collider();
+collider.gameField = new Polygon([[0,0],[cnv.width,0],[cnv.width,cnv.height],[0,cnv.height]]);
+
+// Custom objects
+var someBox = new Polygon([[0,0],[20,0],[20,10],[0,10]]);
+someBox.velocity = new Vector(5,5);
+objects.push(someBox);
+
+var someCircle = new Circle(10, 70, 50);
+someCircle.velocity = new Vector(12,8);
+objects.push(someCircle);
+
+// Adding to collider
+
+collider.addObject.apply(collider, objects);
+
+function gameloop() {
+    ctx.clearRect(0, 0, cnv.width, cnv.height);
+    for (var i = 0; i < objects.length; i++) {
+        var obj = objects[i];
+        obj.stroke(ctx);
+        obj.fill(ctx);
+        obj.move();
+    }
+    collider.collideWithField();
+}
+function run() {
+    tid = setTimeout(function () {
+        if (tid) {
+            run();
+        }
+    }, INTERVAL);
+    gameloop();
+}
+function stop() {
+    clearTimeout(tid);
+    tid = null;
+}
+
+run();
---
+++ src/collider.js
@@ -0,0 +1,16 @@
+var Collider = function () {
+    this.objects = [];
+    this.gameField = null;
+};
+Collider.prototype.addObject = function () {
+    if (arguments.length) {
+        this.objects.push.apply(this.objects, arguments);
+    }
+};
+Collider.prototype.collideWithField = function () {
+    var obj;
+    for (var i = 0; i < this.objects.length; i++) {
+        obj = this.objects[i];
+        obj.collidesWithBorders(this.gameField);
+    }
+};
